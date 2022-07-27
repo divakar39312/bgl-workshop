ecc82a6f79c71f0470866605c2315004b6e43c01
Divakar <divakar39312@gmail.com>
2022-07-27 09:35:56 +0530

    commit added

--- src/com/bgl/tictoctoe/TictacToe.java
+++ src/com/bgl/tictoctoe/TictacToe.java
@@ -22,13 +22,29 @@ public class TictacToe {
 		}while(ch.equals("yes"));
   
 	}
-	
-	
 	public void newBoard() {
+		char posndef[]= {'0','1','2','3','4','5','6','7','8','9'};
+		int i;
+		counter=0;
+		player='X';
+		for(i=1;i<10;i++) posn[i]=posndef[i];
+		currentBoard();
 		
 	}
 	public String currentBoard() {
-		return null;
+		System.out.println( "\n\n" );
+        System.out.println(  "\n\n" );
+        System.out.println(  "\n\n\t\t" + posn [1] + "   | " +posn [2]+ "  | " +posn [3]);
+        System.out.println(  " \t\t    |    |   " );
+        System.out.println(  " \t\t ___|____|___ " );
+        System.out.println(  "\n\n\t\t" +posn [4]+ "   | " +posn [5]+ "  | " +posn [6]);
+        System.out.println(  " \t\t    |    |   " );
+        System.out.println(  " \t\t ___|____|___ " );
+        System.out.println(  "\n\n\t\t" +posn [7]+ "   | " +posn [8]+ "  | " +posn [9]);
+        System.out.println(  " \t\t    |    |   " );
+        System.out.println(  " \t\t    |    |   " );
+        System.out.println(  "\n\n" );
+        return "currentBoard";
 		
 	}
 	public void play() {
