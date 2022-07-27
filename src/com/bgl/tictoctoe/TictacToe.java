129be69e3809aefa0e2f9c12026636f4fa918678
Divakar <divakar39312@gmail.com>
2022-07-27 08:46:19 +0530

    [divakar]:second commit

--- src/com/bgl/tictoctoe/TictacToe.java
+++ src/com/bgl/tictoctoe/TictacToe.java
@@ -1,10 +1,30 @@
 package com.bgl.tictoctoe;
 
 public class TictacToe {
-
+     private int counter;
+     private char posn[]=new char[10];
+     private char player;
+     
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String ch;
+		TictacToe toe=new TictacToe();
+  
 	}
 
+	
+	
+	
+	
+	public void newBoard() {
+		
+	}
+	public String currentBoard() {
+		return null;
+		
+	}
+	public void play() {
+		
+	}
+	
 }
