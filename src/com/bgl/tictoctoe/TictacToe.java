8111ed4a43780fcc3411ea0319ac5b2d44a6add8
Divakar <divakar39312@gmail.com>
2022-07-27 09:02:32 +0530

    [divakar]:uc1 added

--- src/com/bgl/tictoctoe/TictacToe.java
+++ src/com/bgl/tictoctoe/TictacToe.java
@@ -1,5 +1,7 @@
 package com.bgl.tictoctoe;
 
+import java.util.Scanner;
+
 public class TictacToe {
      private int counter;
      private char posn[]=new char[10];
@@ -9,11 +11,17 @@ public class TictacToe {
 		// TODO Auto-generated method stub
 		String ch;
 		TictacToe toe=new TictacToe();
+		do {
+			toe.newBoard();
+			toe.play();
+			System.out.println("like to play again enter yes");
+			Scanner sc=new Scanner(System.in);
+			ch=sc.nextLine();
+			System.out.println("ch value"+ch);
+			
+		}while(ch.equals("yes"));
   
 	}
-
-	
-	
 	
 	
 	public void newBoard() {
